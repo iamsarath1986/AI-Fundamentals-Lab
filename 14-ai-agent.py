285de4c75f288a440bc6d2170546adf8b38362de
iamsarath1986 <sarathiscookie@hotmail.com>
2025-08-26 13:14:11 +0400

    Create AI agent

--- 14-ai-agent.py
+++ 14-ai-agent.py
@@ -1,1 +1,26 @@
 from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatOllama(model="mistral")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"AI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
