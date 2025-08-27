c30c3ea0cddbc7cde67baaea3c915915f1c97625
iamsarath1986 <sarathiscookie@hotmail.com>
2025-08-27 16:05:30 +0400

    Update message

--- 14-ai-agent.py
+++ 14-ai-agent.py
@@ -1,17 +1,19 @@
-from typing import TypedDict, List
-from langchain_core.messages import HumanMessage
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
 
 class AgentState(TypedDict):
-    messages: List[HumanMessage]
+    messages: List[Union[HumanMessage, AIMessage]]
 
 llm = ChatOllama(model="mistral")
 
 def process(state: AgentState) -> AgentState:
     response = llm.invoke(state["messages"])
+    state["messages"].append(AIMessage(content=response.content))
     print(f"AI: {response.content}")
+    print("CURRENT STATE: ", state["messages"])
     return state
 
 graph = StateGraph(AgentState)
@@ -20,7 +22,22 @@ graph.add_edge(START, "process")
 graph.add_edge("process", END)
 agent = graph.compile()
 
+conversation_history = []
 user_input = input("Enter: ")
 while user_input != "exit":
-    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
     user_input = input("Enter: ")
+
+with open("conversation_history.txt", "w") as file:
+    file.write("Your conversation log:\n")
+
+    for conversation in conversation_history:
+        if isinstance(conversation, HumanMessage):
+            file.write(f"You: {conversation.content}\n")
+        elif isinstance(conversation, AIMessage):
+            file.write(f"AI: {conversation.content}\n\n")
+    file.write("End of conversation.\n")
+
+print("Conversation saved to conversation_history.txt:")
